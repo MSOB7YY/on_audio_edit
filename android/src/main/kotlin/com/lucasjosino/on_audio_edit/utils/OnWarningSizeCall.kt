9d3c3e6bb7968f12f9b8912bf1dccf00b6658302
MSOB7YY <mohamedsobhy201566@gmail.com>
2023-07-08 17:36:07 +0300

    update dep

--- android/src/main/kotlin/com/lucasjosino/on_audio_edit/utils/OnWarningSizeCall.kt
+++ android/src/main/kotlin/com/lucasjosino/on_audio_edit/utils/OnWarningSizeCall.kt
@@ -3,17 +3,21 @@ package com.lucasjosino.on_audio_edit.utils
 import android.util.Log
 
 fun warningSizeCall(sizeValue: Long, data: String) {
-    when (true) {
+    when {
         sizeValue >= 13 -> {
-            Log.e("on_audio_warning","-------------------------------------------------------------------------------------")
+            Log.e("on_audio_warning", "-------------------------------------------------------------------------------------")
             Log.e("on_audio_wn_size", "[$data] size is bigger than 13 MB - [$sizeValue MB]")
-            Log.e("on_audio_warning","-------------------------------------------------------------------------------------")
+            Log.e("on_audio_warning", "-------------------------------------------------------------------------------------")
         }
         sizeValue >= 10 -> {
             Log.e("on_audio_warning", "[$data] size is bigger than 10 MB - [$sizeValue MB]")
         }
         sizeValue >= 6 -> {
             Log.i("on_audio_warning", "[$data] size is bigger than 6 MB - [$sizeValue MB]")
+        }
+        else -> {
+            // Handle the case when sizeValue is less than 6
+            Log.i("on_audio_warning", "[$data] size is less than 6 MB - [$sizeValue MB]")
         }
     }
 }
